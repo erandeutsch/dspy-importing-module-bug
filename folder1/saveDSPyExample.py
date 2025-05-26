e734a2569a1fe7d3ff4f2bc338511ffb5c05143a
erandeutsch <eran@earthdynamics.ai>
2025-05-26 16:34:52 -0700

    Cleaned up the print statements

--- folder1/saveDSPyExample.py
+++ folder1/saveDSPyExample.py
@@ -4,12 +4,16 @@ from ExtractInfo import ExtractInfo
 dspy.configure(lm=dspy.LM('ollama_chat/qwen3:4b', api_base='http://localhost:11434', api_key=''))
 
 
-# Save the imported module to a file
 module = dspy.Predict(ExtractInfo)
+print("Running the module directly in folder1:")
+print(module(document="This document is about the city of Paris."))
+
+
+# Save the imported module to a file
 module.save("testModule", save_program=True)
 
 
 # Running the module in the same directory works
+print("Loading and running the module in folder1:")
 loaded_module = dspy.load("testModule")
-print(loaded_module)
 print(loaded_module(document="This document is about the city of Paris."))
--- folder2/loadDSPyExample.py
+++ folder2/loadDSPyExample.py
@@ -7,11 +7,12 @@ dspy.configure(lm=lm)
 
 # Running the module directly works
 module = dspy.Predict(ExtractInfo)
+print("Running the module directly in folder2:")
 print(module(document="This document is about the city of Paris."))
 
 
 # Running the module with load doesn't work in a different directory
 # ModuleNotFoundError: No module named 'ExtractInfo'
+print("Loading and running the module in folder2:")
 loaded_module = dspy.load("testModule")
-print(loaded_module)
 print(loaded_module(document="This document is about the city of Paris."))
